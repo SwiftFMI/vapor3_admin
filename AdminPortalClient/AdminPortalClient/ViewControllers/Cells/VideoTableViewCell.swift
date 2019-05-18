a433a833128858d3ba21c09db5e7c77680aeb0ae
Dimitar Stoyanov <mitko25522@abv.bg>
2019-05-19 02:10:23 +0300

    Added video fetching logic to Client and Server

---
+++ AdminPortalClient/AdminPortalClient/ViewControllers/Cells/VideoTableViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  VideoTableViewCell.swift
+//  AdminPortalClient
+//
+//  Created by Dimitar Stoyanov on 14.05.19.
+//  Copyright © 2019 Dimitar Stoyanov. All rights reserved.
+//
+
+import Foundation
---
+++ AdminPortalClient/AdminPortalClient/ViewControllers/VideosViewController.swift
@@ -0,0 +1,30 @@
+//
+//  VideosViewController.swift
+//  AdminPortalClient
+//
+//  Created by Dimitar Stoyanov on 18.05.19.
+//  Copyright © 2019 Dimitar Stoyanov. All rights reserved.
+//
+
+import UIKit
+
+class VideosViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
